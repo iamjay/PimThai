9465142c7df004adb00b45fad6630d3fff8ed229
Pathompong Puengrostham <pathompong@gmail.com>
2012-05-18 10:37:10 +0700

    Lock screen to landscape mode.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <QtCore>
 #include <QtSql>
 
+#if __QNX__
+#include <stdbool.h>
+#include <sys/platform.h>
+#include "bps/navigator.h"
+#endif
+
 #include "JDummyInputContext.h"
 #include "PimThaiWindow.h"
 
@@ -9,6 +15,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
 #if __QNX__
+    navigator_set_orientation(0, 0);
+    navigator_rotation_lock(true);
     QFontDatabase::addApplicationFont("app/native/fonts/Garuda.ttf");
 #else
     QFontDatabase::addApplicationFont("Garuda.ttf");
