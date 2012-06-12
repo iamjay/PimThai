1673e49ab551d3cfa46384b42c8182abcb12766d
Pathompong Puengrostham <pathompong@gmail.com>
2012-06-12 12:13:40 +0700

    Added GPL license

--- Toaster.h
+++ Toaster.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2012 Pathompong Puengrostham <pathompong@gmail.com>
+ *
+ * This file is part of PimThai.
+ *
+ * PimThai is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * PimThai is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PimThai.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TOASTER_H
 #define TOASTER_H
 
